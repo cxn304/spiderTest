16344bd6caeec895991694dfc68792a9430a056e
cxn304 <33156791+cxn304@users.noreply.github.com>
2017-11-20 21:56:55 +0800

    Update readHtml.py

--- readHtml.py
+++ readHtml.py
@@ -33,12 +33,15 @@ html = req.read()  # str类型数据
 div_bf = BeautifulSoup(html, "html.parser")  # BeautifulSoup类型数据
 div = div_bf.find_all('div', class_='nr')  # ResultSet类型数据
 a = div_bf.find_all('img')  # 找寻照片标签
+
 for each in a:
     urls.append(target+each.get('src'))
+    
 stuIdRegex = re.compile(r'\d\d\d\d\d\d\d\d\d\d\d\d')
 stuId = stuIdRegex.findall(html)  # 学号，一个列表
 dataNumber = len(div)
 stuName = []
+
 for i in range(0, dataNumber-4, 5):
     stuName.append(div[i].text + ' ' + div[i+1].text)
 
